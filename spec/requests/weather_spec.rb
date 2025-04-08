e0c77e2273dbd24c970823a945b48cdb9c254fb8
Steven Chau <steven.p.chau@gmail.com>
2025-04-08 14:23:02 -0700

    Fix specs

--- spec/requests/weather_spec.rb
+++ spec/requests/weather_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe "Weathers", type: :request do
   describe "GET /show" do
     context "with a valid location" do
       it "returns weather for a valid location" do
-        VCR.use_cassette("show_weather", record: :all) do
+        VCR.use_cassette("show_weather") do
           get "/location?query=2651 NE 49th St, Seattle, WA 98105"
 
           expect(response).to render_template(:show)
-          expect(response.body).to include("Widget was successfully created.")
+          expect(response.body).to include("Today's Forecast")
+          expect(response.body).to include("Daily Forecast")
         end
       end
     end
